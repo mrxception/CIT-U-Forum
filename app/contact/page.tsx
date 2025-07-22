98544541b0abe93de885596adb23fee042861673
mrxception <mrxmrx459@gmail.com>
2025-07-23 01:09:01 +0800

    Removed some lines

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -69,7 +69,6 @@ export default function ContactPage() {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        {/* Contact Form */}
         <div className="lg:col-span-2">
           <Card>
             <CardHeader>
