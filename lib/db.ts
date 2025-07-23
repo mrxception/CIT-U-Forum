3d9eb1a395a1a333a0a2cdc2fbeaeabd9a671dd2
mrxception <mrxmrx459@gmail.com>
2025-07-23 11:07:22 +0800

    Revert changes

--- lib/db.ts
+++ lib/db.ts
@@ -5,8 +5,8 @@ const pool = mysql.createPool({
   user: "sql12791346",
   password: "eYH7qsZqU3",
   database: "sql12791346",
-  // waitForConnections: true,
-  // connectionLimit: 10,
+  waitForConnections: true,
+  connectionLimit: 20,
   // queueLimit: 0,
 })
 
